f73e869c886d06541e1275f4480ca4df430458ad
Patrick Loyd <patrickloyd789@gmail.com>
2022-08-29 16:54:14 -0400

    Update API Testing

--- api_test.py
+++ api_test.py
@@ -10,12 +10,13 @@ class TestApi(unittest.TestCase):
         os.environ[cls.environ_name] = "test environment variable"
         cls.api_name = "TestApi"
         cls.api = Api(cls.api_name, cls.environ_name)
-        cls.test_data = "test data"
-        cls.full_path = f"{cls.api_name.upper()}/{cls.api_name.lower()}"
+        cls.test_data = "{}"
+        cls.test_file = "testing"
+        cls.full_path = f"{cls.api_name.upper()}/{cls.test_file}.json"
         print(cls.full_path)
 
     def test_save_to_file(self):
-        self.api.save_to_file(self.test_data)
+        self.api.save_to_file(self.test_data, self.test_file)
         # ensure directory is created
         expected_result = True
         result = os.path.isdir(self.api_name.upper())
--- src/api.py
+++ src/api.py
@@ -4,15 +4,17 @@ import click
 
 class Api:
     def __init__(self, api_name, env_name):
-        self.api_name = api_name.lower()
+        self.api_name = api_name
         self.dir_path = api_name.upper()
         self.env_name = env_name
         self.key = os.environ.get(self.env_name)
-        self.full_path = os.path.join(self.dir_path, self.api_name)
 
-    def save_to_file(self, data):
+    # add file_name parameter
+    def save_to_file(self, data, file_name):
         if not os.path.isdir(self.dir_path):
             os.mkdir(self.dir_path)
 
-        with click.open_file(self.full_path, "w") as f:
+        full_path = os.path.join(self.dir_path, file_name) + ".json"
+
+        with click.open_file(full_path, "w") as f:
             f.write(data)
