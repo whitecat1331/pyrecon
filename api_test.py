ae5136d15f92da3717e24a47522adf655f8a4c97
Patrick Loyd <patrickloyd789@gmail.com>
2022-08-29 21:48:26 -0400

    Add save decorator to api

--- api_test.py
+++ api_test.py
@@ -3,26 +3,40 @@ from src.api import Api
 import os
 
 
+class TApi(Api):
+    def __init__(self):
+        self.environ_name = "testing"
+        os.environ[self.environ_name] = "test environment variable"
+        self.api_name = "TestApi"
+        Api.__init__(self, self.api_name, self.environ_name)
+
+    @Api.save
+    def save_file(self):
+        return "Some data"
+
+
 class TestApi(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
-        cls.environ_name = "testing"
-        os.environ[cls.environ_name] = "test environment variable"
-        cls.api_name = "TestApi"
-        cls.api = Api(cls.api_name, cls.environ_name)
-        cls.test_data = "{}"
+        cls.tapi = TApi()
         cls.test_file = "testing"
-        cls.full_path = f"{cls.api_name.upper()}/{cls.test_file}.json"
-        print(cls.full_path)
+        cls.test_data = "{}"
 
     def test_save_to_file(self):
-        self.api.save_to_file(self.test_data, self.test_file)
+        self.tapi.save_to_file(self.test_data, self.test_save_to_file)
         # ensure directory is created
         expected_result = True
-        result = os.path.isdir(self.api_name.upper())
+        result = os.path.isdir(self.tapi.api_name.upper())
         self.assertEqual(expected_result, result)
 
         # ensure file was created
         expected_result = True
-        result = os.path.exists(self.full_path)
+        result = os.path.exists("TESTAPI/test_save_to_file.json")
+        self.assertEqual(expected_result, result)
+
+    def test_save(self):
+        self.tapi.save_file()
+        expected_result = True
+        result = os.path.exists("TESTAPI/save_file.json")
         self.assertEqual(expected_result, result)
--- src/api.py
+++ src/api.py
@@ -9,12 +9,20 @@ class Api:
         self.env_name = env_name
         self.key = os.environ.get(self.env_name)
 
+    @staticmethod
+    def save(func):
+        def wrapper(self):
+            result = func(self)
+            self.save_to_file(result, func)
+
+        return wrapper
+
     # add file_name parameter
-    def save_to_file(self, data, file_name):
+    def save_to_file(self, data, api_func):
         if not os.path.isdir(self.dir_path):
             os.mkdir(self.dir_path)
 
-        full_path = os.path.join(self.dir_path, file_name) + ".json"
+        full_path = os.path.join(self.dir_path, api_func.__name__) + ".json"
 
         with click.open_file(full_path, "w") as f:
             f.write(data)
